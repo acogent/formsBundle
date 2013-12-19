93ae9222d4016035d0a78d7a856000b65af44640
Armelle Cogent <armelle.cogent@ign.fr>
2013-12-19 11:20:11 +0100

    use de Validator

--- Command/generateFormCommand.php
+++ Command/generateFormCommand.php
@@ -11,17 +11,16 @@
 
 namespace SGN\FormsBundle\Command;
 
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Output\Output;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use SGN\FormsBundle\Generator\SGNDoctrineFormGenerator;
+use SGN\TwigCrudBundle\Command\Validators;
 use Sensio\Bundle\GeneratorBundle\Command\Helper\DialogHelper;
 use Sensio\Bundle\GeneratorBundle\Manipulator\RoutingManipulator;
-
-
-use SGN\FormsBundle\Generator\SGNDoctrineFormGenerator;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\Output;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 /**
  * Generates a CRUD for a Doctrine entity.
  *
