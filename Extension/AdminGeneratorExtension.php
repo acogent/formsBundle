aa8b73d2dfef88bc29b390dbb72e663077fc31e1
Kevin <kevin.eyermann@ign.fr>
2014-02-03 13:28:30 +0100

    Ajout d'un menu pour l'AdminInterface

---
+++ Extension/AdminGeneratorExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace SGN\FormsBundle\Extension;
+
+use SGN\AdminInterfaceBundle\Extension\ExtensionInterface;
+
+class UserAiExtension implements ExtensionInterface
+{
+    public function getMenu()
+    {
+        return array(
+            '<span class="glyphicon glyphicon-hdd"></span> AdminDB' => array(
+                'routename'         => 'sgn_forms_formscrud_show_4',
+                // 'submenu'           => array(
+                //     'List'      => array(
+                //         'routename'         => 'admin_user_list',
+                //         'index'             => 1,
+                //     ),
+                // ),
+            ),
+        );
+    }
+}
