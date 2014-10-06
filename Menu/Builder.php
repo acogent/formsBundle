6e96b45948878fda08177cef01b76d36c3955d8c
Armelle Cogent <armelle.cogent@ign.fr>
2014-10-06 18:24:38 +0200

    Suppression du lien de menu AdminDB qui doit etre dans les bundles 'clients'

--- Menu/Builder.php
+++ Menu/Builder.php
@@ -11,11 +11,11 @@ class Builder extends ContainerAware
     {
         $menu = $factory->createItem('forms_menu');
 
-        $menu->addChild('AdminDB', array(
-            'route' => 'sgn_forms_formscrud_show_4',
-            'label' => '<span class="glyphicon glyphicon-hdd"></span> AdminDB',
-            'extras' => array('safe_label' => true),
-        ));
+        // $menu->addChild('AdminDB', array(
+        //     'route' => 'sgn_forms_formscrud_show_4',
+        //     'label' => '<span class="glyphicon glyphicon-hdd"></span> AdminDB',
+        //     'extras' => array('safe_label' => true),
+        // ));
 
         return $menu;
     }
