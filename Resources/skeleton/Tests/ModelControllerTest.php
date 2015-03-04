ffd5f324eedb4f30362810761b683b54b7f00bdf
Armelle Cogent <armelle.cogent@ign.fr>
2015-03-04 16:30:04 +0100

    Cosmetique

--- Resources/skeleton/Tests/ModelControllerTest.php
+++ Resources/skeleton/Tests/ModelControllerTest.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 //http://jobeet.thuau.fr/testez-vos-formulaires
 class ModelControllerTest extends WebTestCase
 {
-    static public $id  ;
+    public static $ident;
 
     public function setUp()
     {
@@ -17,9 +17,9 @@ class ModelControllerTest extends WebTestCase
         $this->showOne = "/admin/crud/$this->bundle/$this->entity/showone/";
         $this->showJSON = "/admin/crud/$this->bundle/$this->entity/json/show";
 
-        $this->url_new = '/admin/crud/'.$this->bundle.'/'.$this->entity.'/new/';
-        $this->url_edit = '/admin/crud/'.$this->bundle.'/'.$this->entity.'/edit/';
-        $this->url_delete = '/admin/crud/'.$this->bundle.'/'.$this->entity.'/delete/';
+        $this->urlNew = '/admin/crud/'.$this->bundle.'/'.$this->entity.'/new/';
+        $this->urlEdit = '/admin/crud/'.$this->bundle.'/'.$this->entity.'/edit/';
+        $this->urlDelete = '/admin/crud/'.$this->bundle.'/'.$this->entity.'/delete/';
 
         $this->PHP_AUTH_USER = 'A_MODIF_USER';
         $this->PHP_AUTH_PW = 'A_MODIF_PW';
@@ -28,10 +28,10 @@ class ModelControllerTest extends WebTestCase
 
         // environment permet de choisir le fichier config_test.yml
         $this->client = static::createClient(
-            array( 'environment' => 'test','debug' => TRUE, ),
-            array( 'PHP_AUTH_USER' => $this->PHP_AUTH_USER, 'PHP_AUTH_PW'   => $this->PHP_AUTH_PW, )
+            array( 'environment' => 'test', 'debug' => true),
+            array( 'PHP_AUTH_USER' => $this->PHP_AUTH_USER, 'PHP_AUTH_PW'   => $this->PHP_AUTH_PW)
         );
-        $this->kernel = static::createKernel(array('environment'=>'test', 'debug'=>TRUE) ); // ce sont les valeurs par defaut, donc pas utile, c'est juste pour se rappeler que cela peut etre changé
+        $this->kernel = static::createKernel(array('environment' => 'test', 'debug' => true)); // ce sont les valeurs par defaut, donc pas utile, c'est juste pour se rappeler que cela peut etre changé
         $this->kernel->boot();
     }
 
@@ -43,7 +43,7 @@ class ModelControllerTest extends WebTestCase
 
     public function testAuditScenario()
     {
-        $crawler = $this->client->request('GET', $this->showHTML );
+        $crawler = $this->client->request('GET', $this->showHTML);
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET ".$this->showHTML);
         $this->assertTrue($crawler->filter('h3:contains("Audit")')->count() > 0);
     }
@@ -59,38 +59,38 @@ class ModelControllerTest extends WebTestCase
 
     private function initId()
     {
-        $em = $this->kernel->getContainer()->get('doctrine.orm.entity_manager');
-        $query = $em->createQuery($this->sqlMax);
+        $eManager = $this->kernel->getContainer()->get('doctrine.orm.entity_manager');
+        $query = $eManager->createQuery($this->sqlMax);
 
-        self::$id = $query->getSingleScalarResult() > 0 ? $query->getSingleScalarResult() : 1;
+        self::$ident = $query->getSingleScalarResult() > 0 ? $query->getSingleScalarResult() : 1;
     }
     public function testUpdateScenario()
     {
-        $crawler = $this->client->request('GET', $this->url_edit.self::$id.'/');
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET ".$this->url_edit.self::$id.'/');
+        $crawler = $this->client->request('GET', $this->url_edit.self::$ident.'/');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET ".$this->url_edit.self::$ident.'/');
         $form = $crawler->selectButton('Modifier')->form(array( ));
         $this->client->submit($form);
         $this->assertTrue($this->client->getResponse()->isRedirect($this->showHTML));
     }
     public function testShowHtmlScenario()
     {
-        $crawler = $this->client->request('GET', $this->showHTML );
+        $crawler = $this->client->request('GET', $this->showHTML);
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET ".$this->showHTML);
     }
     public function testShowJsonScenario()
     {
-        $crawler = $this->client->request('GET', $this->showJSON );
+        $crawler = $this->client->request('GET', $this->showJSON);
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET ".$this->showJSON);
     }
     public function testShowOneScenario()
     {
-        $crawler = $this->client->request('GET', $this->showOne.self::$id.'/' );
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET ". $this->showOne.self::$id.'/');
+        $crawler = $this->client->request('GET', $this->showOne.self::$ident.'/');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET ".$this->showOne.self::$ident.'/');
     }
     public function testDeleteScenario()
     {
-        $crawler = $this->client->request('GET', $this->url_delete.self::$id.'/');
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET ".$this->url_delete.self::$id.'/');
+        $crawler = $this->client->request('GET', $this->url_delete.self::$ident.'/');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET ".$this->url_delete.self::$ident.'/');
         $form = $crawler->selectButton('Supprimer')->form(array());
         $this->client->submit($form);
         $this->assertTrue($this->client->getResponse()->isRedirect($this->showHTML));
@@ -99,5 +99,4 @@ class ModelControllerTest extends WebTestCase
         unset($form);
         gc_collect_cycles();
     }
-
 }
